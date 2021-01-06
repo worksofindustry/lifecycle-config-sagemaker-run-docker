eab628442d00adff3fe135e4887c0ee5b9618bc5
Matthew Linker <38694245+worksofindustry@users.noreply.github.com>
2021-01-06 12:19:46 -0500

    Update lifecycle_config.sh

--- lifecycle_config.sh
+++ lifecycle_config.sh
@@ -4,6 +4,8 @@ set -e
 CONTAINER_NAME='my_container'
 #The name of the Sagmaker Notebook Instance
 NOTEBOOK_INSTANCE_NAME='my_Notebook'
+#Fetch Scripts
+wget -O /home/ec2-user/SageMaker/monitor_container.sh https://raw.githubusercontent.com/worksofindustry/lifecycle-config-sagemaker-run-docker/main/monitor_container.sh
 
 /home/ec2-user/SageMaker/run_inference.sh $CONTAINER_NAME &>/dev/null &
 
