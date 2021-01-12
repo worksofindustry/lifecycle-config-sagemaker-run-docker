11ee710b62a7acffed61d4320b3736cb8f8c7a4f
Matthew Linker <38694245+worksofindustry@users.noreply.github.com>
2021-01-11 20:07:11 -0500

    Update run_inference.sh

--- run_inference.sh
+++ run_inference.sh
@@ -3,4 +3,4 @@
 aws ecr get-login-password --region <your region> | docker login --username AWS --password-stdin <your_credentials>
 docker pull <your_container>
 #In this example my container is equipted to use SageMaker gpus, also mounting S3 buckets directly to container using s3fs to keep storage requirements minimal
-docker run --cap-add SYS_ADMIN --device /dev/fuse --gpus all --name $1 <your_container>
+docker run --cap-add SYS_ADMIN --runtime=nvidia --device /dev/fuse --gpus all --name $1 <your_container>
